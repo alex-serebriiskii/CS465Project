215d656d3603a4103a878db83fdb95b4ced5de2d
Joseph Hines <jthines3@gmail.com>
2021-06-02 09:44:03 -0400

    Changed ports

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -7,5 +7,5 @@
 
 docker build -t discordium_prod -f Docker/Dockerfile.prod .
 
-docker run --rm -d -p 80:80 -p 443:443 --name discordium_prod discordium_prod
+docker run --rm -d -p 5000:80 -p 5001:443 --name discordium_prod discordium_prod
 
